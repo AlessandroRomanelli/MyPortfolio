03f0797ec0e03714107cf87a2defb58a76e51467
Alessandro Romanelli <aleromrod@gmail.com>
2017-07-26 16:47:46 +0200

    Carousel display fix

---
+++ data/projects.js
@@ -0,0 +1,76 @@
+var projects = [
+  {
+    title:"Random Quote Generator",
+    img:"images/random.jpg",
+    text: "This project was my first programm ever. It consisted of creating a simple JavaScript code that would randomly print out a famous quote to the user, along with author and year. Pretty basic, right?",
+    date: "December 2016"
+  },
+  {
+    title:"Pagination Content Filter",
+    img:"images/pagination.jpg",
+    text: "For my second project I had to enhance an already existing page. I had to use JavaScript to dynamically divide the overly long list of student that was provided to me into pages and add a search function to list only the students matching the search.",
+    date: "January 2017"
+  },
+  {
+    title:"Build an Interactive Form",
+    img:"images/form.jpg",
+    text: "For my third submission I had to use my fresh JavaScript skills to enhance a form so that it would become more engaging, interactive and easy to use for the end-user, making the form change dynamically depending on the user input.",
+    date: "February 2017"
+  },
+  {
+    title:"Tic Tac Toe",
+    img:"images/tictactoe.jpg",
+    text: "My fourth task was to build a fun two-person Tic Tac Toe game. The aim was to use professional programming techniques to solve data-modeling problems, write elegant JavaScript code, and apply higher level concepts like object-oriented programming.",
+    date: "February 2017"
+  },
+  {
+    title:"Build a Search App",
+    img:"images/searchapi.jpg",
+    text: "For this challenge I had to use JavaScript to communicate with an API to rietrieve and display music albums from Spotify. I had to build a form to search and view information and use AJAX request to load data from a server.",
+    date: "March 2017"
+  },
+  {
+    title:"Build a Content Scraper",
+    img:"images/scraper.jpg",
+    text: "This time I had to come up with a data-mining application using Node.js. I created a command-line application that would read a web page, extract information and store it in a text file.",
+    date: "April 2017"
+  },
+  {
+    title:"Build a Twitter Interface",
+    img:"images/twitter.jpg",
+    text: "On this project I was tasked to build and Express application (which runs on Node.js), that would communicate with the Twitter REST API, retrieve JSON data and display the results using a HTML template.",
+    date: "May 2017"
+  },
+  {
+    title:"Building with Gulp",
+    img:"images/gulp.jpg",
+    text: "This project was aimed to develop my skills by automating my workflow, mastering gulp means that I can implement an effective, professional build system using JS, NPM and Node.",
+    date: "May 2017"
+  },
+  {
+    title:"Single Page App",
+    img:"images/recipes.jpg",
+    text: "This time I had to create a seamless user experience by building a single page application using JavaScript and the popular framework AngularJS. The project communicates with a Node.js REST API to add, edit and retrieve cooking recipes.",
+    date: "June 2017"
+  },
+  {
+    title:"Build a Library Manager",
+    img:"images/library.jpg",
+    text: "The goal of this project was to get acquainted with databases, storing, retrieving and deleting information. I had to create a web application for a library based on JavaScript, Node.js, Express and SQLite Database.",
+    date: "June 2017"
+  },
+  {
+    title:"Build a Course Rating API",
+    img:"images/rest.png",
+    text: "The objective this time was to create an API. I used popular web application frameworks such as Express, and MongoDB database to create the back-end for a Course Rating Application, which lets user create, edit and rate courses.",
+    date: "July 2017"
+  },
+  {
+    title:"Build my Web Application",
+    img:"images/mys.jpg",
+    text: "The Capstone Project of my Treehouse Javascript course was to develop my own personal web application from scratch, building all aspects of the website, from the front to the back end.",
+    date: "July 2017"
+  }
+];
+
+module.exports = projects;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var projects = require('../data/projects');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AR - Web Portfolio' });
+  res.render('index', {
+    title: 'AR - Web Portfolio'
+  });
 });
 
 router.get('/about', function(req, res, next) {
@@ -11,7 +14,10 @@ router.get('/about', function(req, res, next) {
 })
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'AR - Projects'})
+  res.render('projects', {
+    title: 'AR - Projects',
+    projects: projects
+  })
 })
 
 module.exports = router;
